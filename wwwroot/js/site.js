40d1767103f17108ae9d6a5775561bea5b364869
GabeFalcon <gfalcon@atu.edu>
2023-10-16 18:35:06 -0500

    Changes and added images to wwwroot folder

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.getElementById('searchIcon').addEventListener('click', function () {
+    document.getElementById('searchBar').classList.toggle('d-none');
+});
