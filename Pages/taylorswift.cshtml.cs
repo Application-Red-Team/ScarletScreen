f89c3affd8a8d5bb29342aac25bbe0068c28e7af
GabeFalcon <gfalcon@atu.edu>
2023-10-17 01:50:40 -0500

    moviepage created and a few updates

---
+++ Pages/taylorswift.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ScarletScreen.Pages
+{
+    public class taylorswiftModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ wwwroot/js/watchnow.js
@@ -0,0 +1,21 @@
+﻿// When the page is ready
+$(document).ready(function () {
+    // When the "Watch Now" button is clicked
+    $("#watchNowBtn").click(function () {
+        // Show the modal
+        $("#streamingModal").css("display", "block");
+
+        // Populate the modal with streaming service options (you can add this dynamically using JavaScript)
+        var streamingServices = ["Netflix", "Amazon Prime", "Hulu", "Disney+", "HBO Max"];
+        var streamingserviceList = $("#streamingserviceList");
+        streamingServices.forEach(function (service) {
+            var listItem = $("<li>").text(service);
+            streamingserviceList.append(listItem);
+        });
+    });
+
+    // When the user clicks on the close button or anywhere outside of the modal, close it
+    $(".close, .modal").click(function () {
+        $("#streamingModal").css("display", "none");
+    });
+});
