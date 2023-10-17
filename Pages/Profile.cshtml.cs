cb52a30af5d7b1e5fc4b500d13bdfa37a20f9295
GabeFalcon <gfalcon@atu.edu>
2023-10-16 23:28:47 -0500

    Profile update

--- Pages/Profile.cshtml.cs
+++ Pages/Profile.cshtml.cs
@@ -2,27 +2,32 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Hosting;
+
+
 
 namespace ScarletScreen.Pages
 {
     public class ProfileModel : PageModel
     {
-        public IActionResult OnGet()
+        public void OnGet()
         {
-            if (User.Identity.IsAuthenticated)
-            {
-                // User is logged in, retrieve and display their profile
-                // Logic to retrieve profile information
-                return Page();
-            }
-            else
-            {
-                // User is not logged in, redirect to account creation
-                return RedirectToPage("/CreateAccount");
-            }
+            //  if (User.Identity.IsAuthenticated)
+            //{
+            // User is logged in, retrieve and display their profile
+            // Logic to retrieve profile information
+            //return Page();
+            // }
+            // else
+            // {
+            // User is not logged in, redirect to account creation
+            // return RedirectToPage("/CreateAccount");
+            //}
         }
-
-
+       
+            public string Username { get; set; } = "Megamind";
+            // Add other properties with placeholder data
+       
 
     }
 }
