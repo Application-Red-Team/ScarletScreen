45c0f5fb91b72aedab61a484381874654b2ea47a
Alexander McCutcheon <snowpuppy4290@gmail.com>
2023-12-10 23:52:59 -0600

    Add the Mongo connection information to the Program.cs at startup.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,26 @@
-using ScarletScreen.Model;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using ScarletScreen.Model;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configuration for appsettings.json
+builder.Configuration.AddJsonFile("appsettings.json");
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+// Add MongoDB
+var mongoConnectionString = builder.Configuration.GetConnectionString("DatabaseSettings:ConnectionString");
+var mongoClient = new MongoClient(mongoConnectionString);
+var mongoDatabaseName = builder.Configuration.GetValue<string>("DatabaseSettings:DatabaseName");
+builder.Services.AddSingleton<IMongoDatabase>(mongoClient.GetDatabase(mongoDatabaseName));
+
+// Add SQL Server
 builder.Services.AddDbContext<AccountsContext>(options =>
 {
     options.UseSqlServer("Server=DESKTOP-Q9342B9;Database=Accounts;Trusted_Connection=True;TrustServerCertificate=True;");
@@ -17,7 +32,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
